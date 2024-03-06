953acdba9e6914098022d4c1c8f6b587b7eaf89d
ALEJANDROCT777 <162204200+ALEJANDROCT777@users.noreply.github.com>
2024-03-06 16:22:21 -0600

    Add files via upload

---
+++ Students/17-AlejandroT/tarea1.py
@@ -0,0 +1,21 @@
+class Alejandro Gonzalez
+
+
+nombre = "Alejandro"
+edad = 19
+estatura = 1.71
+personalidad = "Buena onda, extrovertido y hablador"
+
+print(f"Nombre del Estudiante: {nombre}")
+print(f"Edad: {edad} años")
+print(f"Estatura: {estatura} m")
+print(f"Personalidad: {personalidad}")
+
+class Actividades
+
+fav_1 = "ir al gym"
+fav_2 = "salir con amigos"
+fav_3 = "estudiar"
+fav_4 = "comer"
+
+print("mis actividades favoritas son "+ fav_1 + fav_2 + fav_3"  y tambien "+ fav_4)
