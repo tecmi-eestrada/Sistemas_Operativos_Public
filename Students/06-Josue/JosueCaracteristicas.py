2751a31b2bc289beb1227ec1a050a051179a49e0
JosueAlejandroG <162374535+JosueAlejandroG@users.noreply.github.com>
2024-03-06 16:26:00 -0600

    Add files via upload
    Actividad del 6 de Marzo de 2024, Josue Alejandro

---
+++ Students/06-Josue/JosueCaracteristicas.py
@@ -0,0 +1,23 @@
+class JosueAlejandro():
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def cara(self):
+        print("Tengo una cara reconocible y soy guapo.")
+
+    def pelo(self):
+        print("Tengo un peinado del que siempre recibo comentarios")
+    
+    def energia(self):
+        print("Tengo mucha energia que yo creo que da ventaja por sobre los demas")
+
+    def preparacion(self):
+        print("Este ultimo año me he adelantado en casi todas mis responsabiliades")
+
+josue = JosueAlejandro("Josue Alejandro")
+
+josue.cara()
+josue.pelo()
+josue.energia()
+josue.preparacion()
