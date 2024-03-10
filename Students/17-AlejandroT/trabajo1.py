9636dec2de8c601e8fb1354cd1205ccd637b2a1d
ALEJANDROCT777 <162204200+ALEJANDROCT777@users.noreply.github.com>
2024-03-09 20:41:36 -0600

    Add files via upload
    la volvi a subir profe por lo de que no se habia subido bien

---
+++ Students/17-AlejandroT/trabajo1.py
@@ -0,0 +1,74 @@
+'''
+Restaurante - Clase
+#instancia de la clase restaurante
+fondita_doña chole = Restaurante ("fondita doña chole", "antojitos", 1998, "flautin")
+
+
+   empleados - Clase
+     (nombre, edad, rol)
+
+   gerente=empleados("Rodrigo", 56, "Administrativo", "gerente")
+   jefe_de _cocina= empleados ("Rodrigo", 56, "administrativo", "jefe de cocina")
+    cocinero_1= empleados("Rodrigo", 56, "administrativo", "cocinero")
+Gerente    
+   -jefe de cocina - clase cocineros
+     -cocinero
+     -cocinero2 
+     -cocinero3
+    
+   -jefe de meseros - clase meseros
+    -mesero1
+    -mesero2
+    -mesero3
+
+    -jefe de limpieza - clase limpieza
+      -limpieza1
+      -limpieza2
+      -limpieza3
+    
+    -otros - clase otros
+      -cajero
+      -recepcionista
+      -barista
+'''
+class restaurante 
+ restaurante = restaurante ("mike", 19, "mesero")
+    def_init_(self, nombre, edad, cargo):
+   self.nombre = nombre 
+   self.edad = edad
+   self.cargo = cargo
+
+class Cocinero
+    def __init__(self, nombre, edad, cargo):
+
+class Mesero
+    def __init__(self, nombre, edad, cargo):
+
+class Limpieza
+    def __init__(self, nombre, edad, cargo):
+
+class Otros
+    def __init__(self, nombre, edad, cargo):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
