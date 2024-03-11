f07ef91269835bbcd1b5f48067db55d7e177b4ff
ALEJANDROCT777 <162204200+ALEJANDROCT777@users.noreply.github.com>
2024-03-11 16:31:06 -0600

    Add files via upload

---
+++ Students/17-AlejandroT/ejemplo.py
@@ -0,0 +1,45 @@
+#INACTIVE
+TRABAJANDO =0
+SIN_HACER_NADA =1
+EN_DESCANSO =2
+
+def registro_de_estados(state):
+    if state == TRABAJANDO:
+        print("trabajando")
+        return state
+    elif state == SIN_HACER_NADA:
+        print("sin hacer nada")
+        return state
+    elif state ==EN_DESCANSO:
+        print("en descanso")
+        return state
+    
+
+def registro_de_control(process, state):
+    my_state = registro_de_estados(state)
+    if state == SIN_HACER_NADA:
+        print(f"puede atender, esta desocupado {process}, estado {my_state}")
+        return 0
+    if state == TRABAJANDO:
+        print(f"no puede atender, esta ocupado {process}, estado {my_state}")
+        return 1
+    if state == EN_DESCANSO:
+        print(f"no puede atender, esta en descanso {process}, estado {my_state}")
+        return 2
+
+def registro_de_datos(data, state, process):
+    control =registro_de_control(process, state)
+    if control ==1:
+        print(f"se le informo al {data}")
+    if control ==0:
+        print(f"se le informo al {data}")
+    if control ==2:
+        print(f"se le informo al {data} ")
+
+if __name__ == "__main__":
+    mi_tarea = "Supervisor"
+    registro_de_datos(mi_tarea, SIN_HACER_NADA, "para tomar pedido")
+ 
+
+
+
